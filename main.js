591fe19a1abead9d684de1d5c086e58758ec06f2
aviralsingh108 <admiralaviral@gmail.com>
2023-02-19 00:00:54 +0530

    collision condiiton added

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ function animate() {
   handleObstacles();
   bird.update();
   bird.draw();
+  handleCollisons();
   handleParticles();
   requestAnimationFrame(animate);
   angle += 0.12;
@@ -40,3 +41,19 @@ window.addEventListener("keyup", (e) => {
 
 const bang = new Image();
 bang.src = "bang.png";
+
+function handleCollisons() {
+  for (let index = 0; index < obstaclesArray.length; index++) {
+    if (
+      bird.x < obstaclesArray[index].x + obstaclesArray[index].width &&
+      bird.x + bird.width > obstaclesArray[index].x &&
+      ((bird.y < 0 + obstaclesArray[index].top && bird.y + bird.height > 0) ||
+        (bird.y > canvas.height - obstaclesArray[index].bottom &&
+          bird.y + bird.height < canvas.height))
+    ) {
+      //collison detected
+      ctx.drawImage(bang, bird.x, bird.y, 50, 50);
+      return true;
+    }
+  }
+}
